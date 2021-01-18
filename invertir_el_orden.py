70be39498ed3f1407b35b0aff87e7d24cb71a68f
Michel-Corona <michel.carmia@gmail.com>
2021-01-18 17:24:27 -0600

    Uso de indices negativos para invertir el orden de un array

---
+++ invertir_el_orden.py
@@ -0,0 +1,4 @@
+import array as arr
+Mi_Array=arr.array('i',[1,2,3,4,5])
+#[::-1] reimprime una copia inversa de las estructuras de datos ordenadas.La matriz o lista original permanece sin cambios.
+print (Mi_Array[::-1])
