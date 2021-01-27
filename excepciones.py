00f6be76a313c7880302dd399f84876d21f1d948
Michel-Corona <michel.carmia@gmail.com>
2021-01-27 00:54:13 -0600

    Manejo de excepciones utilizando try y except

---
+++ excepciones.py
@@ -0,0 +1,14 @@
+"""Creamos una función en donde cada elemento de 
+una lista es dividida por un divisor definido"""
+def divide_elementos_de_lista(lista, divisor):
+    try:
+        return [i / divisor for i in lista]
+    except ZeroDivisionError as e:
+        print(e)
+        return lista
+
+
+lista = list(range(10))
+divisor = 0
+
+print(divide_elementos_de_lista(lista, divisor))
