3c3d967eb22f75751de9e32aab4c37da3f063708
Michel-Corona <michel.carmia@gmail.com>
2021-01-27 00:02:08 -0600

    Ejemplo de range

---
+++ rangos.py
@@ -0,0 +1,41 @@
+def pares(numero):
+    """ Imprime todos los numeros pares del 0 hasta n o de n hasta 0
+
+        int n 
+        returns n    
+    """
+
+    if numero < 0 and numero % 2 != 0:
+        for i in range(numero+1, 0, 2):
+            print(i)
+    elif numero < 0 and numero % 2 == 0:
+        for i in range(numero, 0, 2):
+            print(i)
+    else:
+        for i in range(0, numero, 2):
+            print(i)
+
+def impares(numero):
+    """imprime todo los numeros impares desde 1 hasta n o desde n hasta 1 
+    
+        int n
+        returns n
+    """
+    if numero < 0 and numero % 2 == 0:
+        for i in range(numero+1, 1, 2):
+            print(i)
+    elif numero < 0 and numero % 2 != 0:
+        for i in range(numero, 1, 2):
+            print(i)
+    else:
+        for i in range(1, numero, 2):
+            print(i)
+
+if __name__ == '__main__':
+    num = int(input('Escribe un entero: '))
+
+    impares(num)
+    print('Numeros Impares\n')
+
+    pares(num)
+    print('Numeros Pares')
